49d14411b931687713c88a2da101f17d8e79120d
Buddhi DeSilva <buddhi.desilva@gmail.com>
2012-05-03 01:08:07 +0800

    Temporary disable banners

--- app/overrides/homepager_hooks.rb
+++ app/overrides/homepager_hooks.rb
@@ -1,8 +1,8 @@
-Deface::Override.new(:virtual_path => "spree/layouts/admin",
-                     :name => "banner_admin_tab",
-                     :insert_bottom => "[data-hook='admin_tabs']",
-                     :text => "<%= tab(:banners) %>",
-                     :disabled => false)
+# Deface::Override.new(:virtual_path => "spree/layouts/admin",
+#                      :name => "banner_admin_tab",
+#                      :insert_bottom => "[data-hook='admin_tabs']",
+#                      :text => "<%= tab(:banners) %>",
+#                      :disabled => false)
 
 Deface::Override.new(:virtual_path => "spree/admin/products/_form",
                     :name => "ext_homepager_admin_product_fields",
